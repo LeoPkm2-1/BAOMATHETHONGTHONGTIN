f73b53183defabb12d9933a4bfae2bd06636e50f
spitliner <73539619+spitliner@users.noreply.github.com>
2023-04-09 19:00:27 +0000

    Update 3_autofill_cccd_trigger.sql

--- sql/3_autofill_cccd_trigger.sql
+++ sql/3_autofill_cccd_trigger.sql
@@ -1,10 +1,12 @@
-CREATE TRIGGER AutoFillCCCD
+CREATE OR REPLACE TRIGGER AutoFillCCCD
     BEFORE INSERT ON congdan
+    REFERENCING NEW AS NEW OLD AS OLD
     FOR EACH ROW
     WHEN (new.cccd IS NULL)
 DECLARE
     max_cccd NUMBER := 1;
 BEGIN
-    SELECT MAX(cccd) FROM congdan INTO max_cccd;
-    :new.cccd := max_cccd + 1;
+    SELECT MAX(cccd) INTO max_cccd FROM congdan;
+    :new.cccd := COALESCE(max_cccd + 1,1);
 END;
+/
