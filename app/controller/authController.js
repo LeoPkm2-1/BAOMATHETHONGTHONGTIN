88d81a57657edd29f8e840cf2ae4ac59b54275da
spitliner <>
2023-04-10 02:51:12 +0700

    set up hash alg

---
+++ app/controller/authController.js
@@ -0,0 +1,16 @@
+import argon2, { argon2id } from 'argon2';
+
+class AuthController {
+    static async hashPassword(password) {
+        return argon2.hash(password, {
+            type: argon2.argon2id,
+            memoryCost: 2 ** 16,
+            timeCost: 4,
+            hashLength: 100,
+        });
+    }
+
+    static async verifyPassword(password, hash) {
+        return argon2.verify(hash, password);
+    }
+}
